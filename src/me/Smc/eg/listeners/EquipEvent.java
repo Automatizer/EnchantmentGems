99f3f821d8aff04c889599eb967a06881507fd3c
BuildTools <unconfigured@null.spigotmc.org>
2018-04-24 21:57:20 -0400

    removed unnecessary methods

--- src/me/Smc/eg/listeners/EquipEvent.java
+++ src/me/Smc/eg/listeners/EquipEvent.java
@@ -34,41 +34,4 @@ public class EquipEvent implements Listener{
 		}
 	}
 	
-	/*@EventHandler
-	public void onRightClickArmor(PlayerInteractEvent e){
-		if(e.getAction().equals(Action.RIGHT_CLICK_AIR) || e.getAction().equals(Action.RIGHT_CLICK_BLOCK))
-			if(e.getPlayer().getItemOnCursor() != null) EnchantManager.callEvent(e.getPlayer().getInventory().getItemInMainHand(), "onEquip", e.getPlayer(), null, 0.0, null);
-	}
-	
-	@EventHandler
-	public void onArmorEquip(InventoryClickEvent e){
-		Player player = (Player) e.getWhoClicked();
-		ItemStack toCheck = e.getCurrentItem();
-		if(toCheck == null || toCheck.getType().equals(Material.AIR)) return;
-		if(!e.isShiftClick()) {
-			EnchantManager.callEvent(toCheck, "onEquip", player, null, 0.0, null);
-			if(EnchantManager.hasEnchant(toCheck, "leaping")) EnchantManager.callEvent(toCheck, "onLeap", player, null, 1.0, null);
-		}
-	}
-	
-	@EventHandler
-	public void onArmorUnequip(InventoryClickEvent e) {
-		Player player = (Player) e.getWhoClicked();
-		ItemStack is = e.getCurrentItem();
-		SlotType st = e.getSlotType();
-		if(st == SlotType.ARMOR) {
-			if(!(is == null || is.getType().equals(Material.AIR))){
-				for(Enchant en : EnchantManager.getEnchants(is)){
-					if(en.getName().equalsIgnoreCase("nightvision")){
-						if(player.hasPotionEffect(PotionEffectType.NIGHT_VISION)) player.removePotionEffect(PotionEffectType.NIGHT_VISION);
-					}else if(en.getName().equalsIgnoreCase("flash")) {
-						if(player.hasPotionEffect(PotionEffectType.SPEED)) player.removePotionEffect(PotionEffectType.SPEED);
-					}else if(en.getName().equalsIgnoreCase("leaping")) {
-						if(player.hasPotionEffect(PotionEffectType.JUMP)) player.removePotionEffect(PotionEffectType.JUMP);
-					}
-				}
-			}
-		}
-	}*/
-	
 }
